1ff17ae0a0e606bec344519e1b7f7ebbe296eea6
Andrii <andriyslb@gmail.com>
2023-04-05 19:00:47 +0300

    new

---
+++ Controllers/CheckPersonInEpisode.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace API_assignment.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/check-person")]
+    public class CheckPersonController : ControllerBase
+    {
+        private readonly string _baseUrl = "https://rickandmortyapi.com/api/";
+
+        [HttpPost]
+        public async Task<IActionResult> CheckPersonInEpisode([FromBody] CheckPersonRequest request)
+        {
+            var characterUrl = $"{_baseUrl}/character/?name={request.PersonName}";
+            var httpClient = new HttpClient();
+            var characterResponse = await httpClient.GetAsync(characterUrl);
+
+            if (!characterResponse.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+
+            var characterContent = await characterResponse.Content.ReadAsStringAsync();
+            var characterJson = JsonDocument.Parse(characterContent);
+            var characterResults = characterJson.RootElement.GetProperty("results");
+
+            if (characterResults.GetArrayLength() == 0)
+            {
+                return NotFound();
+            }
+
+            var characterId = characterResults[0].GetProperty("id").GetInt32();
+            var episodeUrl = $"{_baseUrl}/episode/?name={request.EpisodeName}";
+            var episodeResponse = await httpClient.GetAsync(episodeUrl);
+
+            if (!episodeResponse.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+
+            var episodeContent = await episodeResponse.Content.ReadAsStringAsync();
+            var episodeJson = JsonDocument.Parse(episodeContent);
+            var episodeResults = episodeJson.RootElement.GetProperty("results");
+
+            if (episodeResults.GetArrayLength() == 0)
+            {
+                return NotFound();
+            }
+
+            var episodeCharacters = episodeResults[0].GetProperty("characters");
+
+            foreach (var character in episodeCharacters.EnumerateArray())
+            {
+                var url = character.GetString();
+
+                if (url.Contains(characterId.ToString()))
+                {
+                    return Ok(true);
+                }
+            }
+
+            return Ok(false);
+        }
+    }
+    public class CheckPersonRequest
+    {
+        public string? PersonName { get; set; }
+        public string? EpisodeName { get; set; }
+    }
+}
+
+
+
+
---
+++ Controllers/GetCharacter.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace API_assignment.Controllers
+{
+    [ApiController]
+    [Route("api/v1/person")]
+    public class CheckCharacter : ControllerBase
+    {
+        private readonly string _baseUrl = "https://rickandmortyapi.com/api/";
+
+        [HttpGet]
+        public async Task<IActionResult> GetCharacter(string name)
+        {
+            var characterUrl = $"{_baseUrl}/character/?name={name}";
+            var httpClient = new HttpClient();
+            var characterResponse = await httpClient.GetAsync(characterUrl);
+
+            if (!characterResponse.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+
+            var characterContent = await characterResponse.Content.ReadAsStringAsync();
+            var characterJson = JsonDocument.Parse(characterContent);
+            var results = characterJson.RootElement.GetProperty("results")[0];
+
+            var id = results.GetProperty("id").GetInt32();
+            var dimension = results.GetProperty("name").GetString();
+            var status = results.GetProperty("status").GetString();
+            var species = results.GetProperty("species").GetString();
+            var type = results.GetProperty("type").GetString();
+            var gender = results.GetProperty("gender").GetString();
+            var origin = results.GetProperty("origin");
+            var originName = origin.GetProperty("name").GetString();
+            var originUrl = origin.GetProperty("url").GetString();
+
+            var result = new
+            {
+                id,
+                dimension,
+                status,
+                species,
+                type,
+                gender,
+                origin = new { dimension = originName, url = originUrl }
+            };
+
+            return Ok(result);
+        }
+    }
+}
